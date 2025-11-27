76f114026ecb51687f12080f8c98ef1def4815b2
Raphael Castelar <castelaraphael@gmail.com>
2025-11-27 11:26:10 -0300

    controllers and CORS

---
+++ src/main/java/com/example/login_auth_api/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.example.login_auth_api.controllers;
+
+
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Sucesso, o endpoint está protegido!");
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+}
